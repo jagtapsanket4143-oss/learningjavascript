6e6ad34d552824b85f6b37c48859b559881480d3
Sanket Jagtap <jagtapsanket4143@gmail.com>
2026-04-20 21:14:47 +0530

    new variables

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("This is y first javascript program!!");
+console.log("This is first javascript program!!");
+
+
+let emailaddress =97;
+const fName ="Sanket Jagtap";
+console.log(emailaddress);
+console.log(fName);
+emailaddress = "sanketjagt@gamail.com";
+console.log(emailaddress);
